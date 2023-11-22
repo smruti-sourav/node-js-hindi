0eb17e69785d1976ae1cecba35d95b89bc460ee8
@smrutisourav <151321398+smruti-sourav@users.noreply.github.com>
2023-11-22 18:55:58 +0000

    convertion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,71 @@
+let score = "sourav"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "sourav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+//************************operator***********************************'
+
+
+
+let Value = 4
+let negValue = -Value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "sourav"
+let str = " smruti"
+
+str3 = str1+str
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
